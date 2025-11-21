40f81550219ea849bd73cb57b14ec6f282aa6266
daihoangnguyen-duancongty <daihoangnguyen17101994@gmail.com>
2025-11-21 17:32:37 +0700

    fix admin-web - lan 6

---
+++ admin-web/src/index.tsx
@@ -0,0 +1,11 @@
+// // src/index.tsx
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { AdminApp } from './App';
+
+// const container = document.getElementById('admin-web-root');
+// if (container) {
+//   ReactDOM.createRoot(container).render(<AdminApp />);
+// } else {
+//   console.error('Không tìm thấy #admin-web-root');
+// }
